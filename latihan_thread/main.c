10590852571209b1a7e7d255551f3c8678cedf56
Mahdy Syafiqa Jannata <mahdy.jannata@delameta.com>
2021-07-16 10:02:00 +0700

    Tugas Thread Kelipatan

--- latihan_thread/main.c
+++ latihan_thread/main.c
@@ -6,15 +6,39 @@
 // bikin thread
 void *tesThread(void *gatau){
 	while(1){
-		printf("tes doang 1\n");
+		long * tes = gatau;
+		printf("Kelipatan 2 %ld\n", *tes);
+		sleep(2);
+	}
+	return NULL;
+}
+
+void *tes2Thread(void *gatau){
+	while(1){	
+		long * tes = gatau;
+		printf("Kelipatan 3 %ld\n", *tes);
+		sleep(3);
+	}
+	return NULL;
+}
+
+void *tes3Thread(void *gatau){
+	while(1){	
+		long * tes = gatau;
+		printf("Kelipatan 5 %ld\n", *tes);
+		sleep(5);
 	}
 	return NULL;
 }
 
 
 int main(void){
-	pthread_t trit1;
-	pthread_create(&trit1, NULL, tesThread, NULL);
+	pthread_t trit1, trit2, trit3;
+	pthread_create(&trit1, NULL, tesThread, (void *) &trit1);
+	pthread_create(&trit2, NULL, tes2Thread, (void *) &trit2);
+	pthread_create(&trit3, NULL, tes3Thread, (void *) &trit3);
 	pthread_join(trit1, NULL);
-	exit(0);
+	pthread_join(trit2, NULL);
+	pthread_join(trit3, NULL);
+	pthread_exit(0);
 }
