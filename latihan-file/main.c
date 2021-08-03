38ffbae9c67ff473ef067e42b9a35c5a2affe14f
Mahdy Syafiqa Jannata <mahdy.jannata@delameta.com>
2021-08-03 13:32:54 +0700

    updet fungsi

--- latihan-file/main.c
+++ latihan-file/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h> 
 
 // char* _keyword_, char* _value_
-int baca_file(char* _namafile_, char* _keyword_, char* _value_){
+int baca_file(char* _namafile_, char* _keyword_, char** _value_){
 
     
-    char temp[50];
+    char temp[150];
     int8_t tanda_ketemu = 0;
+    bool cek = false;
+    int8_t index = 0;
 
     
     /**
@@ -31,6 +34,7 @@ int baca_file(char* _namafile_, char* _keyword_, char* _value_){
         printf("File terbuka\n\n");
 
 
+        memset(temp,0,sizeof(temp));
 
         /**
          * @brief Mulai pencarian dengan fungsi fgets
@@ -41,31 +45,51 @@ int baca_file(char* _namafile_, char* _keyword_, char* _value_){
 
             if((strstr(temp,_keyword_)) != NULL){
                 
-                if((strstr(temp,_value_)) != NULL){
 
-                    printf("Pencarian ditemukan:\n");
-                    printf("%s\n", temp);
+                // for(int i=0; i<strlen(temp); i++){
+                //     if(temp[i] == '='){
+                //         cek = true;
+                //         index=0;
+                //         continue;
+                //     }
+                //     if(cek != false){
+                //         temp_value[index] = temp[i];
+                //         index++;
+                //     }
+                // }
 
-                }
+                // printf("%s\n", temp_value);
+                int posisi = strlen(_keyword_)+1;
+                printf("%d\n", posisi);
                 
+                char temp_value[50];
+                memset(temp_value,0,50);
+                
+                memcpy(temp_value,temp+posisi,strlen(temp)-posisi);
+                printf("ini value : %s\n", temp_value);
+                
+                memcpy(*_value_,temp_value,strlen(temp_value));
+
+
+                memset(temp,0,sizeof(temp));
                 /**
                  * @brief Kasih penanda kalo ketemu biar kalo
                  * ga ditemukan by keyword nanti tanda_ketemu = 0
                  * 
                  */
                 tanda_ketemu++;
-
             }
+            memset(temp,0,sizeof(temp));
+
         }
         if(!tanda_ketemu){
             printf("Keyword tidak ditemukan\n");
             return 1;
         }
-
+        
+        fflush(fpointer);
         fclose(fpointer);
-
     }
-
 }
 
 
@@ -75,7 +99,13 @@ int main(int argc, char* argv[]){
         printf("argumen kurang! berikut parameternya ./tes <dirfile> <keyword>\ncontoh:\n\n./tes conf/db.conf PCDS\n\n");
         return -1;
     }
-    baca_file(argv[1],argv[2],argv[3]);
+
+    char *value;
+
+    if(!baca_file(argv[1],argv[2],&value)){
+        printf("%s",value);
+    }
+
     return 0;
 
 }
